48703a9aee9055f755921741d76684a26bc004f5
Daniel <danieltoledo790@gmail.com>
2021-10-30 00:59:50 -0500

    Primer commit

---
+++ app/src/main/java/com/danieltm/pruevarepositorio/Activity2.java
@@ -0,0 +1,20 @@
+package com.danieltm.pruevarepositorio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+
+    public void retornar(View f){
+        finish();
+    }
+}
---
+++ app/src/main/java/com/danieltm/pruevarepositorio/MainActivity.java
@@ -0,0 +1,25 @@
+package com.danieltm.pruevarepositorio;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void acercaDE(View b){
+        Intent siguiente = new Intent(this, Activity2.class);
+        startActivity(siguiente);
+
+    }
+
+
+}
